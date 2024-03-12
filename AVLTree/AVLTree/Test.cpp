c7e650f478031bc73651c5daa50a66a1e496c5a2
杨枫 <2867108693@qq.com>
2024-03-12 11:26:47 +0800

    AVLTree

---
+++ AVLTree/AVLTree/Test.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+using namespace std;
+#include<iostream>
+#include<assert.h>
+#include"AVLTree.h"
+
+
+
+int main()
+{
+	TestAVLTree1();
+	TestAVLTree2();
+	return 0;
+}
