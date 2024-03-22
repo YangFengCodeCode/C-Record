276899e4aa76f613a43bc17005f8296aa97c870c
杨枫 <2867108693@qq.com>
2024-03-22 09:54:26 +0800

    HashTable-1

---
+++ HashTable-1/HashTable-1/Test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+using namespace std;
+#include<iostream>
+using namespace std;
+#include<iostream>
+#include<string>
+#include<vector>
+#include"HashiTable.h"
+
+int main()
+{
+	open_address::TestHT1();
+	open_address::TestHT2();
+	return 0;
+}
+
+
+
