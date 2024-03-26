1f8de5e522ba1afe9ca641e1f935b35f1fa5efa8
杨枫 <2867108693@qq.com>
2024-03-26 19:04:16 +0800

    myMap&&mySet

---
+++ myMap_mySet/myMap_mySet/Test.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+using namespace std;
+#include<iostream>
+#include"myMap.h"
+#include"mySet.h"
+
+
+int main()
+{
+	yf::test_set1();
+	yf::test_map1();
+	yf::test_map2();
+
+	return 0;
+}
